5424087e50adb00de048a1a3fc63904ff0d97239
Natalia Aspee <n.aspeedelgado@gmail.com>
2023-05-15 23:39:40 -0400

    css done, inputs with required

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const form = document.getElementById('form');
-const firsName = document.getElementById('firstName');
+const firstName = document.getElementById('firstName');
 const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const password = document.getElementById('psw');
 const input = document.querySelectorAll('input');
-console.log(input)
+const error = document.querySelectorAll('span');
+
+// console.log(input)
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+})
