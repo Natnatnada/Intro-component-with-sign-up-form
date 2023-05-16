40cd7f9ddb107c8aabff310547528ccc3da56fef
Natalia Aspee <n.aspeedelgado@gmail.com>
2023-05-15 22:37:39 -0400

    adding CSS style, beginning with js for managing errors

---
+++ script.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('form');
+const firsName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const input = document.querySelectorAll('input');
+console.log(input)
